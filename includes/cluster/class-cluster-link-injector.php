5fd35522b3d8437a2c731321a9ffe2e3fd29db49
LogicDuke <fierensstefan08@gmail.com>
2026-02-21 18:39:10 +0100

    Harden cluster link injector safeguards

--- includes/cluster/class-cluster-link-injector.php
+++ includes/cluster/class-cluster-link-injector.php
@@ -13,6 +13,7 @@ class TMW_Cluster_Link_Injector {
 
     public function inject_missing_links($cluster_id) {
         $cluster_id = (int) $cluster_id;
+        $max_links_per_post = 5;
 
         if ($cluster_id <= 0) {
             return [];
@@ -27,6 +28,8 @@ class TMW_Cluster_Link_Injector {
         $updated = 0;
         $processed_pairs = [];
 
+        $missing_links_by_source = [];
+
         foreach ($analysis['missing_links'] as $missing_link) {
             $from_id = isset($missing_link['from']) ? (int) $missing_link['from'] : 0;
             $to_id = isset($missing_link['to']) ? (int) $missing_link['to'] : 0;
@@ -35,30 +38,54 @@ class TMW_Cluster_Link_Injector {
                 continue;
             }
 
-            $pair_key = $from_id . ':' . $to_id;
-            if (isset($processed_pairs[$pair_key])) {
-                continue;
-            }
+            $missing_links_by_source[$from_id][] = $to_id;
+        }
 
+        foreach ($missing_links_by_source as $from_id => $target_ids) {
             $post = get_post($from_id);
 
             if (!$post) {
                 continue;
             }
 
-            $target_url = get_permalink($to_id);
-            $target_title = get_the_title($to_id);
+            if ($post->post_status !== 'publish') {
+                continue;
+            }
 
-            $new_content = $post->post_content . '<p><a href="' . esc_url($target_url) . '">' . esc_html($target_title) . '</a></p>';
+            if (!in_array($post->post_type, ['post', 'page'], true)) {
+                continue;
+            }
 
-            $result = wp_update_post([
-                'ID' => $post->ID,
-                'post_content' => $new_content,
-            ], true);
+            $existing_links = substr_count($post->post_content, '<a href=');
+            if ($existing_links >= $max_links_per_post) {
+                continue;
+            }
 
-            if (!is_wp_error($result) && $result) {
-                $updated++;
-                $processed_pairs[$pair_key] = true;
+            foreach ($target_ids as $to_id) {
+                $pair_key = $from_id . ':' . $to_id;
+                if (isset($processed_pairs[$pair_key])) {
+                    continue;
+                }
+
+                $target_url = get_permalink($to_id);
+                if (strpos($post->post_content, $target_url) !== false) {
+                    continue;
+                }
+
+                $target_title = get_the_title($to_id);
+
+                $new_content = $post->post_content . '<p><a href="' . esc_url($target_url) . '">' . esc_html($target_title) . '</a></p>';
+
+                $result = wp_update_post([
+                    'ID' => $post->ID,
+                    'post_content' => $new_content,
+                ], true);
+
+                if (!is_wp_error($result) && $result) {
+                    $updated++;
+                    $processed_pairs[$pair_key] = true;
+                    break;
+                }
             }
         }
 
