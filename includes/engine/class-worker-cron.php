700ad8790ff46a77a8ef11a41de61c4669253433
LogicDuke <fierensstefan08@gmail.com>
2026-02-23 10:27:29 +0100

    [TMW-SEO-AUTO] Add WorkerCron class for 10-minute queue processing

---
+++ includes/engine/class-worker-cron.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace TMWSEO\Engine;
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class WorkerCron {
+
+    public static function init(): void
+    {
+        add_filter('cron_schedules', [self::class, 'register_interval']);
+        add_action('tmwseo_process_queue', [self::class, 'process_queue']);
+    }
+
+    public static function activate(): void
+    {
+        if (!wp_next_scheduled('tmwseo_process_queue')) {
+            wp_schedule_event(time(), 'tmwseo_every_ten_minutes', 'tmwseo_process_queue');
+        }
+    }
+
+    public static function deactivate(): void
+    {
+        wp_clear_scheduled_hook('tmwseo_process_queue');
+    }
+
+    public static function register_interval(array $schedules): array
+    {
+        $schedules['tmwseo_every_ten_minutes'] = [
+            'interval' => 600,
+            'display'  => 'Every 10 Minutes',
+        ];
+
+        return $schedules;
+    }
+
+    public static function process_queue(): void
+    {
+        // Prevent overlapping executions
+        if (get_transient('tmwseo_worker_lock')) {
+            return;
+        }
+
+        set_transient('tmwseo_worker_lock', 1, 300); // 5 minute lock
+
+        $max_jobs = 5;
+
+        for ($i = 0; $i < $max_jobs; $i++) {
+
+            if (!class_exists('\\TMWSEO\\Engine\\Jobs')) {
+                break;
+            }
+
+            if (!class_exists('\\TMWSEO\\Engine\\Worker')) {
+                break;
+            }
+
+            $job = \TMWSEO\Engine\Jobs::next();
+
+            if (!$job) {
+                break;
+            }
+
+            \TMWSEO\Engine\Worker::dispatch($job);
+        }
+
+        delete_transient('tmwseo_worker_lock');
+    }
+}
