2c6a65d3ca4c5e9d544c8922c03ef358b3f4c5af
LogicDuke <fierensstefan08@gmail.com>
2026-05-27 13:29:00 +0200

    [TMW-SEO-AUTO] Add category keyword CSV dry-run admin preview

--- includes/class-plugin.php
+++ includes/class-plugin.php
@@ -345,6 +345,7 @@ class Plugin {
             \TMWSEO\Engine\Admin\LinkGraphAdminPage::init();
             \TMWSEO\Engine\Admin\TopicMapsAdminPage::init();
             \TMWSEO\Engine\Admin\CSVManagerAdminPage::init();
+            \TMWSEO\Engine\Admin\CategoryKeywordCsvDryRunAdminPage::init();
             \TMWSEO\Engine\Admin\KeywordMetricsCsvImporter::init(); // 5.9.0
             \TMWSEO\Engine\Admin\AIContentBriefGeneratorAdmin::init();
             \TMWSEO\Engine\Admin\SEOEngineRunner::init();
